7906cd6d45dca9f58e161e353cc3eb3649b61905
unknown <tbrown@TerryBrown-HP.telogical.com>
2014-11-07 14:13:32 -0600

    Refactoring Server and folder structure

--- PiCARS.js
+++ PiCARS.js
@@ -6,27 +6,25 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+var PiCARS = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+PiCARS.set('views', path.join(__dirname, 'views'));
+PiCARS.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+PiCARS.use(logger('dev'));
+PiCARS.use(bodyParser.json());
+PiCARS.use(bodyParser.urlencoded({ extended: false }));
+PiCARS.use(cookieParser());
+PiCARS.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+PiCARS.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+PiCARS.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -36,8 +34,8 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+if (PiCARS.get('env') === 'development') {
+    PiCARS.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -48,7 +46,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+PiCARS.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -57,4 +55,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+module.exports = PiCARS;
